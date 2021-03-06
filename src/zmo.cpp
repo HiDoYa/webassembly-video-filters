5338cac9459d7dd65b03320a1a04479e0553a48d
HiDoYa <hiroyagojo@gmail.com>
2021-03-06 11:45:29 -0800

    Change malloc/free to wasi

--- src/zmo.cpp
+++ src/zmo.cpp
@@ -16,14 +16,3 @@ extern "C" KEEPALIVE void rgbparade(unsigned char data[], unsigned char data2[],
 	ZMO::Scope_u8_C4(selector, data, width * 4, data2, width * 4 * 3, r);
 }
 
-// FUNCTIONS TO COMMUNICATE B/W WASM AND JS
-extern unsigned char __heap_base;
-unsigned char *bump_pointer = &__heap_base;
-extern "C" KEEPALIVE void* custom_malloc(int n) {
-	unsigned char *r = bump_pointer;
-	bump_pointer += n;
-	return (void *)r;
-}
-extern "C" KEEPALIVE void custom_free(void* p, int n) {
-	bump_pointer -= n;
-}
