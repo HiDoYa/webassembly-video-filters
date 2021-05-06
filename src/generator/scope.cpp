e4d90b4bdfcde3db1945d34092edd37a0ba8b050
Seth Damany <64705672+sethdamany@users.noreply.github.com>
2021-05-05 18:04:08 -0700

    in-class demo ready

--- src/generator/scope.cpp
+++ src/generator/scope.cpp
@@ -20,25 +20,17 @@ public:
 		output(x, y, c) = cast<uint8_t>(0);
 		output(x, y, 3) = cast<uint8_t>(255);
 		
-		RDom r = RDom(0, input.dim(0).extent(), 0, input.dim(1).extent());
+		RDom r = RDom (0, input.dim(0).extent(), 0, input.dim(1).extent());
 
 		//Value computed in RGB format, assuming R:c==0, G:c==1, B:c==2
-		Expr value0 = input(r.x, r.y, 0);
-		Expr value1 = input(r.x, r.y, 1);
-		Expr value2 = input(r.x, r.y, 2);
-		
-		Expr bucket0 = clamp(cast<uint8_t>(value0), 0, 255);
-		Expr bucket1 = clamp(cast<uint8_t>(value1), 0, 255);
-		Expr bucket2 = clamp(cast<uint8_t>(value2), 0, 255);
+		Expr value = (0.2126f * input(r.x, r.y, 0) + 0.7152f * input(r.x, r.y, 1) + 0.0722f * input(r.x, r.y, 2));
+		Expr bucket = clamp(cast<uint8_t>(value), 0, 255);
 
-		output(r.x + (input.dim(0).extent() * 0), 255 - bucket0, 0) += cast<uint8_t>(50);
-		output(r.x + (input.dim(0).extent() * 1), 255 - bucket1, 1) += cast<uint8_t>(50);
-		output(r.x + (input.dim(0).extent() * 2), 255 - bucket2, 2) += cast<uint8_t>(50);
-		
-		output.dim(0).set_extent(input.dim(0).extent() * 3);
+		output(r.x, 255 - bucket, 1) += cast<uint8_t>(16); 
+
+		output.dim(0).set_extent(input.dim(0).extent());
 		output.dim(1).set_extent(255);
 		output.dim(2).set_extent(input.dim(2).extent());
-
 	}
 
 	void schedule()
@@ -195,9 +187,9 @@ public:
 		Expr bucket1 = clamp(cast<uint8_t>(value1), 0, 255);
 		Expr bucket2 = clamp(cast<uint8_t>(value2), 0, 255);
 
-		output(r.x + (input.dim(0).extent() * 0), 255 - bucket0, 0) += cast<uint8_t>(50);
-		output(r.x + (input.dim(0).extent() * 1), 255 - bucket1, 1) += cast<uint8_t>(50);
-		output(r.x + (input.dim(0).extent() * 2), 255 - bucket2, 2) += cast<uint8_t>(50);
+		output(r.x + (input.dim(0).extent() * 0), 255 - bucket0, 0) += cast<uint8_t>(16);
+		output(r.x + (input.dim(0).extent() * 1), 255 - bucket1, 1) += cast<uint8_t>(16);
+		output(r.x + (input.dim(0).extent() * 2), 255 - bucket2, 2) += cast<uint8_t>(16);
 		
 		output.dim(0).set_extent(input.dim(0).extent() * 3);
 		output.dim(1).set_extent(255);
@@ -615,48 +607,48 @@ public:
 			output.dim (0).set_stride (Expr());
 			input.dim (0).set_stride (Expr());
 
-			Expr planar = (output.dim (0).stride() == 1 && input.dim (0).stride() == 1);
+			// Expr planar = (output.dim (0).stride() == 1 && input.dim (0).stride() == 1);
 
-			Expr packed_uv = (output.dim (0).stride() == 2
-							  && output.dim (2).stride() == 1
-							  && output.dim (2).min() == 0
-							  && output.dim (2).extent() == 2
-							  && input.dim (0).stride() == 2
-							  && input.dim (2).stride() == 1
-							  && input.dim (2).min() == 0
-							  && input.dim (2).extent() == 2);
+			// Expr packed_uv = (output.dim (0).stride() == 2
+			// 				  && output.dim (2).stride() == 1
+			// 				  && output.dim (2).min() == 0
+			// 				  && output.dim (2).extent() == 2
+			// 				  && input.dim (0).stride() == 2
+			// 				  && input.dim (2).stride() == 1
+			// 				  && input.dim (2).min() == 0
+			// 				  && input.dim (2).extent() == 2);
 
-			Expr packed_rgb = (output.dim (0).stride() == 3
-							   && output.dim (2).stride() == 1
-							   && output.dim (2).min() == 0
-							   && output.dim (2).extent() == 3
-							   && input.dim (0).stride() == 3
-							   && input.dim (2).stride() == 1
-							   && input.dim (2).min() == 0
-							   && input.dim (2).extent() == 3);
+			// Expr packed_rgb = (output.dim (0).stride() == 3
+			// 				   && output.dim (2).stride() == 1
+			// 				   && output.dim (2).min() == 0
+			// 				   && output.dim (2).extent() == 3
+			// 				   && input.dim (0).stride() == 3
+			// 				   && input.dim (2).stride() == 1
+			// 				   && input.dim (2).min() == 0
+			// 				   && input.dim (2).extent() == 3);
 
-			Expr packed_rgba = (output.dim (0).stride() == 4
-								&& output.dim (2).stride() == 1
-								&& output.dim (2).min() == 0
-								&& output.dim (2).extent() == 4
-								&& input.dim (0).stride() == 4
-								&& input.dim (2).stride() == 1
-								&& input.dim (2).min() == 0
-								&& input.dim (2).extent() == 4);
+			// Expr packed_rgba = (output.dim (0).stride() == 4
+			// 					&& output.dim (2).stride() == 1
+			// 					&& output.dim (2).min() == 0
+			// 					&& output.dim (2).extent() == 4
+			// 					&& input.dim (0).stride() == 4
+			// 					&& input.dim (2).stride() == 1
+			// 					&& input.dim (2).min() == 0
+			// 					&& input.dim (2).extent() == 4);
 
-			output.specialize (planar);
+			// output.specialize (planar);
 
-			output.specialize (packed_uv)
-			.reorder (c, xi, yi, t)
-			.unroll (c);
+			// output.specialize (packed_uv)
+			// .reorder (c, xi, yi, t)
+			// .unroll (c);
 
-			output.specialize (packed_rgb)
-			.reorder (c, xi, yi, t)
-			.unroll (c);
+			// output.specialize (packed_rgb)
+			// .reorder (c, xi, yi, t)
+			// .unroll (c);
 
-			output.specialize (packed_rgba)
-			.reorder (c, xi, yi, t)
-			.unroll (c);
+			// output.specialize (packed_rgba)
+			// .reorder (c, xi, yi, t)
+			// .unroll (c);
 		}
 	}
 };
