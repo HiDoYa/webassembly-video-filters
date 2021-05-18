501c70751052fe1131f89d0cc8be06e395b4841e
Seth Damany <64705672+sethdamany@users.noreply.github.com>
2021-05-17 19:19:09 -0700

    test cpp color luma (errors)

--- src/benchmark/benchmark.cpp
+++ src/benchmark/benchmark.cpp
@@ -3,35 +3,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "../cpp_scope_helpers.h"
+#include "../cpp_zmo.h"
 
 using namespace std::chrono;
 
-#define HEIGHT 256
-#define WIDTH 128
+// #define WIDTH 128
+// #define HEIGHT 256
 
-char* createData(int width, int height);
+int WIDTH = 128;
+int HEIGHT = 256;
+
+char* create_data(int width, int height);
 
 int main(void)
 {
     typedef high_resolution_clock Time;
     typedef milliseconds ms;
     typedef duration<float> fsec;
+    
+    // create input and output data
+    char* data_in = create_data(WIDTH, HEIGHT);
+    char* data_out = create_data(WIDTH, HEIGHT);
+
+    // get start time
     auto t0 = Time::now();
 
-    for (int i = 0; i < 65556; i++) {
-        // do nothing
-        printf("%d ", i);
-    }
+    // operation
+    cpp_color_lumascope(data_in, data_out, WIDTH, HEIGHT);
 
-    printf("\n");
-
+    // get end time
     auto t1 = Time::now();
+
+    // report findings
     fsec fs = t1 - t0;
     ms d = duration_cast<ms>(fs);
     printf("Duration (ms): %" PRId64 "\n", d.count());
 }
 
-char* createData(int width, int height) {
+char* init_data(int width, int height) {
     int index;
 
     // initialize data
@@ -40,7 +49,7 @@ char* createData(int width, int height) {
     // populate data with random values
     for (int w = 0; w < width; w++) {
         for (int h = 0; h < height; h++) {
-            index = get_index(w, h, width);
+            index = get_index(w, h, height);
             data[index] = (char)(rand() % 256);
         }
     }
